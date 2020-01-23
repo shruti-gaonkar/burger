57c12179a95d3d56f9ce4b7466c142313e7882e1
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-01-22 16:40:49 -0800

    Implementing the handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger");
 
-
+// Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    burger.selectAll();
-})
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", burgers);
+    });
+});
