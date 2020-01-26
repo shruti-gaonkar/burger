254be39cce3ed7fa3712f840ec0251063fb81092
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-01-25 17:22:15 -0800

    Debugging heroku setup

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,6 @@ const mysql = require("mysql");
 const connection;
 
 if (process.env.JAWSDB_URL) {
-    console.log(process.env.JAWSDB_URL);
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
